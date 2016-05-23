d5d64bc13cdc55ecbbd0c8fa27c02981256839f6
Lorin Thwaits <lorint@gmail.com>
2016-05-23 11:26:49 +0100

    First commit -- test accumulator

---
+++ calculator_spec.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require './calculator'
+
+class CalculatorSpec < MiniTest::Test
+	def test_accumulator
+		my_calculator = Calculator.new
+		my_calculator.accumulator = 5
+		assert_equal(my_calculator.accumulator, 5)
+	end
+end
+
