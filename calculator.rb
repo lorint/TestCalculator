9693d9a74afc153960c6079974aa7dd18633783f
Lorin Thwaits <lorint@gmail.com>
2016-05-23 12:35:01 +0100

    Implemented divide by zero functionality

--- calculator.rb
+++ calculator.rb
@@ -1,24 +1,27 @@
 class Calculator
-	attr_reader :accumulator
+  attr_reader :accumulator
 
-	def accumulator=(val)
-		@accumulator = val.to_f
-	end
+  def accumulator=(val)
+    @accumulator = val.to_f
+  end
 
-	def add(val)
-		self.accumulator += val
-	end
+  def add(val)
+    self.accumulator += val
+  end
 
-	def subtract(val)
-		self.accumulator -= val
-	end
+  def subtract(val)
+    self.accumulator -= val
+  end
 
-	def multiply(val)
-		self.accumulator *= val
-	end
+  def multiply(val)
+    self.accumulator *= val
+  end
 
-	def divide(val)
-		return nil if val == 0
-		self.accumulator /= val
-	end
+  def divide(val)
+    if val == 0
+      @accumulator = nil
+      return nil
+    end
+    self.accumulator /= val
+  end
 end
